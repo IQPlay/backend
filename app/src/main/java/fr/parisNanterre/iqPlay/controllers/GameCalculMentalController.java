8c70149031e0aacc7eafc882d6c6ba785baeb0cd
Sami Letellier <42002121@parisnanterre.fr>
2024-12-07 11:01:00 +0100

    fix: retrait de sentry, et ajout de la version 0.2-snapshot de la librairie iqplay

--- app/src/main/java/fr/parisNanterre/iqPlay/controllers/GameCalculMentalController.java
+++ app/src/main/java/fr/parisNanterre/iqPlay/controllers/GameCalculMentalController.java
@@ -5,7 +5,6 @@ import fr.parisNanterre.iqPlay.models.Operation;
 import fr.parisNanterre.iqPlay.models.Response;
 import fr.parisNanterre.iqPlay.services.GameCalculMentalService;
 import fr.parisNanterre.iqPlay.services.OperationService;
-import io.sentry.Sentry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
