a4c10cc3ecda75bbc21bfbf6567e37f16263bd56
Sami Letellier <42002121@parisnanterre.fr>
2025-01-30 09:47:00 +0100

    Ajout des statistiques pour le dashboard utilisateur

--- app/src/main/java/fr/parisnanterre/iqplay/controller/StatistiquesController.java
+++ app/src/main/java/fr/parisnanterre/iqplay/controller/StatistiquesController.java
@@ -1,26 +1,19 @@
 package fr.parisnanterre.iqplay.controller;
 
-import fr.parisnanterre.iqplay.dto.*;
+import fr.parisnanterre.iqplay.entity.GameSessionPersistante;
 import fr.parisnanterre.iqplay.exception.UnauthorizedException;
-import fr.parisnanterre.iqplay.model.GameCalculMental;
-import fr.parisnanterre.iqplay.model.Response;
 import fr.parisnanterre.iqplay.repository.GameSessionRepository;
-import fr.parisnanterre.iqplay.service.GameCalculMentalService;
-import fr.parisnanterre.iqplay.service.OperationService;
 import fr.parisnanterre.iqplay.service.PlayerService;
 import fr.parisnanterre.iqplaylib.api.*;
 import fr.parisnanterre.iqplay.dto.StatsDashboardResponseDto;
 
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * REST controller for managing mental calculation game sessions.
- */
 @CrossOrigin
 @RestController
 @RequestMapping("/api/stats")
@@ -35,22 +28,24 @@ public class StatistiquesController {
         this.gameSessionRepository = gameSessionRepository;
     }
 
-    /**
-     *
-     */
-    @PostMapping("/dashboard")
+    @GetMapping("/dashboard")
     public ResponseEntity<StatsDashboardResponseDto> getStatisticsForDashboard() {
         try {
             IPlayer player = playerService.getCurrentPlayer();
 
-            StatsDashboardResponseDto dashboardResponseDto = new StatsDashboardResponseDto(1, 1, 1, 1);
+            List<GameSessionPersistante> gameSessionPersistantes = this.gameSessionRepository.findAllByPlayer(player);
+
+            int nbGames = gameSessionPersistantes.size();
+            int avgScore = gameSessionPersistantes.stream().mapToInt(GameSessionPersistante::getScore).sum() / nbGames;
+            int bestScore = gameSessionPersistantes.stream().mapToInt(GameSessionPersistante::getScore).max().orElse(0);
+            int serieDay = 0;
+
+            StatsDashboardResponseDto dashboardResponseDto = new StatsDashboardResponseDto(nbGames, avgScore, bestScore, serieDay);
 
             return ResponseEntity.ok(dashboardResponseDto);
 
         } catch (UnauthorizedException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    new StatsDashboardResponseDto(1, 1, 1, 1)
-            );
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
 
