16a41c1ea663576649ed6f2125598f12b6166f3b
Sami Letellier <42002121@parisnanterre.fr>
2025-03-27 19:46:29 +0100

    Ajout du modele et wrapper prize

--- app/src/main/java/fr/parisnanterre/iqplay/model/leaderboard/Prize.java
+++ app/src/main/java/fr/parisnanterre/iqplay/model/leaderboard/Prize.java
@@ -1,4 +1,60 @@
 package fr.parisnanterre.iqplay.model.leaderboard;
 
-public class Prizes {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Prize {
+    private String description;
+    private String name;
+    private String id;
+    private String imgUrl;
+    private int credits;
+
+    public Prize() { }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Prize{" +
+                "description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", credits=" + credits +
+                '}';
+    }
 }
--- app/src/main/java/fr/parisnanterre/iqplay/model/leaderboard/wrapper/PrizeWrapper.java
+++ app/src/main/java/fr/parisnanterre/iqplay/model/leaderboard/wrapper/PrizeWrapper.java
@@ -1,4 +1,18 @@
 package fr.parisnanterre.iqplay.model.leaderboard.wrapper;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import fr.parisnanterre.iqplay.model.leaderboard.Prize;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PrizeWrapper {
+    private List<Prize> prizes;
+
+    public List<Prize> getPrizes() {
+        return prizes;
+    }
+
+    public void setPrizes(List<Prize> prizes) {
+        this.prizes = prizes;
+    }
 }
